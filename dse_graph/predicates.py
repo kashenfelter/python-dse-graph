e0ee744fd603b398436f57aeae2e19d406644146
Alan Boudreault <alan@alanb.ca>
2016-11-14 11:50:11 -0500

    initial search predicates

---
+++ dse_graph/predicates.py
@@ -0,0 +1,59 @@
+# Copyright 2016 DataStax, Inc.
+#
+# Licensed under the DataStax DSE Driver License;
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+# http://www.datastax.com/terms/datastax-dse-driver-license-terms
+
+
+from gremlin_python.process.traversal import P
+
+
+class Search(object):
+
+    @staticmethod
+    def token(value):
+        """
+        Search any instance of a certain token within the text property targeted.
+
+        :param value: the value to look for.
+        """
+        return P('token', value)
+
+    @staticmethod
+    def token_prefix(value):
+        """
+        Search any instance of a certain token prefix withing the text property targeted.
+
+        :param value: the value to look for.
+        """
+        return P('tokenPrefix', value)
+
+    @staticmethod
+    def token_regex(value):
+        """
+        Search any instance of the provided regular expression for the targeted property.
+
+        :param value: the value to look for.
+        """
+        return P('tokenRegex', value)
+
+    @staticmethod
+    def prefix(value):
+        """
+        Search for a specific prefix at the beginning of the text property targeted.
+
+        :param value: the value to look for.
+        """
+        return P('prefix', value)
+
+    @staticmethod
+    def regex(value):
+        """
+        Search for this regular expression inside the text property targeted.
+
+        :param value: the value to look for.
+        """
+        return P('regex', value)
